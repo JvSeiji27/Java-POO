00d74576b63b97c9db24e0e8d06e916248475d10
João Vitor Seiji Sato Calvaro <joaovssc@gmail.com>
2024-06-13 16:34:18 -0300

    Consumer Implementation - In a new Class

--- src/main/java/application/UsingConsumer.java
+++ src/main/java/application/UsingConsumer.java
@@ -3,26 +3,26 @@ package application;
 import entities.Product;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
-import util.ProductPredicate;
 
-public class Programm {
+public class UsingConsumer {
     public static void main(String[] args){
         List <Product> list = new ArrayList<>();
         
         Product p1 = new Product("TV", 800.00);
         Product p2 = new Product("Microwave", 700.00);
         Product p3 = new Product("Apple", 0.50);
-
+        Product p4 = new Product("Mouse", 40.0);
+        
         list.add(p1);
         list.add(p2);
         list.add(p3);
-        Predicate <Product> pred = p -> p.getPrice() >= 100.00;//Posso pegar valores de variáveis
-        list.removeIf(pred);
-        //list.removeIf(Product :: nonStatic);
-        //list.removeIf(p -> p.getPrice() >= 100.00); InLine;
-        for (Product p : list){
+        list.add(p4);
+        
+        
+        list.forEach(new PriceUpdate());
+        list.forEach(System.out::println);
+        /*for(Product p : list){
             System.out.println(p);
-        }
+        }*/
     }
 }
